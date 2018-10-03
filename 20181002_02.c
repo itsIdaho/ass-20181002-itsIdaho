047ca57f62c411c537d58fb9ff12e9e75918f19e
Jan <43406245+itsIdaho@users.noreply.github.com>
2018-10-03 08:19:07 +0200

    Update 20181002_02.c

--- 20181002_02.c
+++ 20181002_02.c
@@ -1,1 +1,24 @@
-
+int
+main (void)
+{
+int a;
+int b;
+a = 0;
+b = 0;
+printf ("Geben Sie die erste Zahl ein: ");
+scanf ("%d", &a);
+printf ("Geben Sie die zweite Zahl ein:");
+scanf ("%d", &b);
+if (a < b)
+    {      
+printf ("%d < %d\n", a, b);    
+}  
+if (a > b)
+    {
+printf ("%d > %d\n", a, b);   
+}
+if (a == b)
+    {
+printf ("%d == %d\n", a, b);   
+}
+}
