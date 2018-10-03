df93616ef51e383f1448fb2bcba425bec3d83a09
Jan <43406245+itsIdaho@users.noreply.github.com>
2018-10-03 08:45:23 +0200

    Update 20181002_03.c

--- 20181002_03.c
+++ 20181002_03.c
@@ -1,1 +1,11 @@
-
+int main (void)
+{
+int a;
+a = 0;
+printf ("Geben Sie die Zahl ein, von der Sie herunterzaehlen möchten: ");
+scanf ("%d", &a);
+{
+    unsigned int counter= a;
+    while(counter--) {
+    printf("%d\n" ,counter);    }}
+}
