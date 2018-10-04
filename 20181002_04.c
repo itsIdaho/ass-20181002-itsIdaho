59931aadddb7fef3f7f81006b9d3d3ffc3d9b86b
Jan <43406245+itsIdaho@users.noreply.github.com>
2018-10-04 16:31:01 +0200

    Update 20181002_04.c

--- 20181002_04.c
+++ 20181002_04.c
@@ -1,83 +1,13 @@
+/**************
+x=Basis
+y=Hochzahl
+*************/
+
 #include <stdio.h>
-  
-int
-exp (int x, int y)
+
+int main()
 {
-  
-int z = x;
-  
- 
-for (int i = 1; i < y; i++)
-    {
-      
-z = z * y;
-    
-} 
- 
-return z;
-
-}
-
-
- 
-int
-main ()
-{
-  
-int x = 0;
-  
-int y = 0;
-  
- 
-printf ("Geben Sie die zu berechnende Zahl ein.\n");
-  
-scanf ("%d", &x);
-  
-printf ("Nun die Hochzahl\n");
-  
-scanf ("%d", &y);
-  
- 
-if (y > 1)
-    {
-      
-printf ("%d^%d ist %d", x, y, exp (x, y));
-    
-}
-  
-  else if (y < -1)
-    {
-      
-y = -y;
-      
- 
-float result = exp (x, y);
-      
-float endResult = 1 / result;
-      
- 
-y = -y;
-      
-printf ("%d^%d ist %f", x, y, endResult);
-    
-}
-  
-  else if (y == 1)
-    {
-      
-printf ("%d^%d ist %d", x, y, x);
-    
-}
-  
-  else
-    {
-      
-printf ("%d^%d ist 1", x, y);
-    
-}
-  
- 
- 
-return 0;
-
+    int x = 0;
+    int y = 0;
+    return 0;
 }
