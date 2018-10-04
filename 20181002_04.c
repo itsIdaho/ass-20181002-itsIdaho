34d57151039f29fda63e8a7204bb25bcf5f8a7a2
Jan <43406245+itsIdaho@users.noreply.github.com>
2018-10-04 16:35:41 +0200

    Update 20181002_04.c

--- 20181002_04.c
+++ 20181002_04.c
@@ -9,5 +9,29 @@ int main()
 {
     int x = 0;
     int y = 0;
+    int power;
+    int i;
+    
+    printf ("Basis angeben\n");
+    scanf ("%d", &x);
+    
+    printf ("Nun die Hochzahl\n");
+    scanf ("%d", &y);
+    
+    power = 1;
+    i = 1;
+    
+    while (i <= y)
+    
+    {
+        power = power * x;
+        i++;
+        
+    }
+    
+    printf ("%d^%d ist %d\n", power)
+    
     return 0;
+    
+}
 }
