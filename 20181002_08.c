44f833417f7b9eef949ef8285f4cf1d5e63379cb
Jan <43406245+itsIdaho@users.noreply.github.com>
2018-10-09 14:55:22 +0200

    Update 20181002_08.c

--- 20181002_08.c
+++ 20181002_08.c
@@ -1,29 +1,18 @@
-/*Nicht sicher ob wir Zahl reinschreiben (printf; scanf) sollen oder eine im Code vorgeben sollen
-*/
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
-int main() {
-    int pruef; 
-    int zahl;
-    int a = 0;
-    
-    printf ("Geben Sie eine Zahl an\n");
-    scanf ("%d", &a);
-    
-    zahl = &a;
-    
-    for(zahl=3; zahl<20; zahl++) 
-    {
-    for(pruef=2; pruef<zahl; pruef++) 
-    {
-    if(zahl % pruef == 0) 
-    {
-    printf("Primzahl: %d\n", zahl);
-    }
-        
-    }}
-    
-    return 0;
+
+int main()
+{
+	unsigned long long num1 = 0;
+	unsigned long long num2 = 0;
+
+	for (num1 = 2; num1 <= 18446744073709551615; num1++) {
+		for (num2 = 2; num2 <= num1; num2++) {
+			if (num1 % num2 == 0 && num1 != num2) break;
+
+			if (num2 == num1) {
+				printf("Primzahl: %d\n", num1);
+			}
+		}
+	}
+	return 0;
 }
